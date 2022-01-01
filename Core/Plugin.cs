6c893581b5cdd31a0d735f2ba533535b96136435
fiote <murilo.eng@gmail.com>
2022-01-01 11:49:55 -0300

    xxx - first commit

---
+++ Core/Plugin.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+namespace NoTips {
+	[BepInPlugin("fiote.mods.notips", "NoTips", "0.0.1")]
+
+	public class NoTips : BaseUnityPlugin {
+		private void Awake() {
+			Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), "fiote.mods.notips");
+		}
+	}
+}
---
+++ Patches/Raven.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+namespace NoTips.Patches {
+
+	[HarmonyPatch(typeof(Raven), "Spawn")]
+	
+	public class Raven_Spawn_Patch {
+		[HarmonyPrefix]
+		internal static bool Prefix(Raven __instance, Raven.RavenText text, bool forceTeleport) {
+			if (text.m_key.Length > 0 && Player.m_localPlayer.HaveSeenTutorial(text.m_key)) {
+				Player.m_localPlayer.SetSeenTutorial(text.m_key);
+			}
+			return false;
+		}
+	}
+
+}
